46789a8a9ce6aa1b2d1c8e06a3fb25ab7c4da10d
massargi <giancarlo.alves@basf.com>
2022-09-15 15:24:12 -0300

    Completed class 9

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,21 @@ import SmoothieCard from "../components/SmoothieCard"
 const Home = () => {
   const [fetchError, setFetchError] = useState(null)
   const [smoothies, setSmoothies] = useState(null)
+  const [orderBy, setOrderBy] = useState('created_at')
+  const [asc, setAsc] = useState(true)
+
+  const handleDelete = (id) => {
+    setSmoothies(prevSmoothies => {
+      return prevSmoothies.filter(sm => sm.id !== id)
+    })
+  }
 
   useEffect(() => {
     const fetchSmoothies = async () => {
       const { data, error } = await supabase
       .from('smoothies')
       .select()
+      .order(orderBy, { ascending: asc })
 
       if(error) {
         setFetchError('Could not fetch the smoothies')
@@ -25,17 +34,31 @@ const Home = () => {
     }
 
     fetchSmoothies()
-  }, [])
+  }, [orderBy, asc])
 
   return (
     <div className="page home">
       { fetchError && (<p>{fetchError}</p>) }
       { smoothies && (
         <div className="smoothies">
-          {/* order by buttons */}
+          <div className="order-by">
+            <p>Order by: {orderBy}</p>
+            <button onClick={() => setOrderBy('created_at')}>Time Created</button>
+            <button onClick={() => setOrderBy('title')}>Title</button>
+            <button onClick={() => setOrderBy('rating')}>Rating</button>
+          </div>
+          <div className="order-by">
+            <p>Ascending: {asc ? 'ascending' : 'descending'}</p>
+            <button onClick={() => setAsc(true)}>Ascending</button>
+            <button onClick={() => setAsc(false)}>Descending</button>
+          </div>
           <div className="smoothie-grid">
             {smoothies.map(smoothie => (
-              <SmoothieCard smoothie={smoothie} key={smoothie.id}/>
+              <SmoothieCard 
+                smoothie={smoothie} 
+                key={smoothie.id}
+                onDelete={handleDelete}
+              />
             ))}
           </div>
         </div>
